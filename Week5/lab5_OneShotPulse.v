7fb50b276d097036666bfd82dab8cde77d548f04
sicajc <jackyyeh1999@gmail.com>
2022-05-07 14:07:08 +0800

    modified

--- Week5/lab5_OneShotPulse.v
+++ Week5/lab5_OneShotPulse.v
@@ -14,28 +14,28 @@ module lab5(
 	reg [20:0] count;
 	wire d_clk;
 
-	// reg btn_c_press_flag; //????
+	// reg btn_c_press_flag; //按壓旗號
 	reg btn_u_press_flag;
 	reg btn_lw_press_flag;
 	reg btn_lft_press_flag;
 	reg btn_ri_press_flag;
 
-	// wire btn_c_pulse; // ?桐???
+	// wire btn_c_pulse; // 單一脈衝
 	wire btn_u_pulse;
 	wire btn_lw_pulse;
 	wire btn_lft_pulse;
 	wire btn_ri_pulse;
 
 
-	reg signed[8:0] total_value; //Used to store the total value so far, remember we must put the value into seg7 temp before we want to display them
-	//reg [7:0] press_count; //??甈⊥閮??
+	reg signed[10:0] total_value; //Used to store the total value so far, remember we must put the value into seg7 temp before we want to display them
+	//reg [7:0] press_count; //按壓次數計數器
 
-	reg signed [7:0] seg7_temp[0:3]; //Used to store the value for seg7 to display
+	reg [8:0] seg7_temp[0:3]; //Used to store the value for seg7 to display
 	reg [1:0] seg7_count; // Used to display seg7 in a fast speed.
 
 	parameter MINUS = 10 ;
 
-	// ?日
+	// 除頻
 	always @(posedge clk or negedge rst_n)begin
 		if(!rst_n)begin
 			count <= 0;
@@ -47,10 +47,10 @@ module lab5(
 				count <= count + 1;
 		end
 	end
-	assign d_clk = count >= (`CYCLE/2) ? 1 : 0;  // ??銝挾憿舐內?函???
+	assign d_clk = count >= (`CYCLE/2) ? 1 : 0;  // 掃描七段顯示器的時脈
 
-	// 頧 ?銝????One pulse shot generator for btns
-	always @(posedge clk or negedge rst_n)begin
+	// 轉為 「單一脈衝」 One pulse shot generator for btns
+	always @(posedge d_clk or negedge rst_n)begin
 		if(!rst_n)
 		begin
 			// btn_c_press_flag <= 0;
@@ -75,7 +75,7 @@ module lab5(
 	assign btn_lft_pulse = {btn_lft,btn_lft_press_flag} == 2'b10 ? 1 : 0;
 	assign btn_ri_pulse = {btn_ri,btn_ri_press_flag} == 2'b10 ? 1 : 0;
 
-	// 閮????活??
+	// 記錄按壓的次數
 	// always @(posedge clk or negedge rst_n)begin
 	// 	if(!rst_n)begin
 	// 		press_count <= 0;
@@ -89,7 +89,7 @@ module lab5(
 	// end
 
 	//Calulate the total value added so far
-	always @(posedge clk or negedge rst_n)
+	always @(posedge d_clk or negedge rst_n)
 	begin
 		if(!rst_n)
 		begin
@@ -110,8 +110,8 @@ module lab5(
 		end
 	end
 
-	// 撠?憯活?貉??詨頧?脖?
-	always @(posedge clk or negedge rst_n)begin
+	// 將按壓次數計數器轉為十進位
+	always @(posedge d_clk or negedge rst_n)begin
 		if(!rst_n)begin
 			seg7_temp[0] <= 0;
 			seg7_temp[1] <= 0;
@@ -128,13 +128,13 @@ module lab5(
 		else
 		begin
 			seg7_temp[3] <= MINUS;
-			seg7_temp[2] <= (total_value % 1000) / 100;
-			seg7_temp[1] <= (total_value % 100) / 10;
-			seg7_temp[0] <= total_value % 10;
+			seg7_temp[2] <= ((~total_value + 1) % 1000) / 100;
+			seg7_temp[1] <= ((~total_value + 1)  % 100) / 10;
+			seg7_temp[0] <=  (~total_value + 1) % 10;
 		end
 	end
 
-	//憿舐內?潔?畾菟＊蝷箏
+	//顯示於七段顯示器
 	always @(posedge d_clk or negedge rst_n)begin
 		if(!rst_n)begin
 			seg7_count <= 0;
